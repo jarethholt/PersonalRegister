1322bd84b93db6d535eaf5ff572f3b1bef38e515
jarethholt <jareth.holt@gmail.com>
2024-02-27 16:23:16 +0100

    feat: Added next steps
    Added comments about what steps to take next.

--- PersonalRegister/Program.cs
+++ PersonalRegister/Program.cs
@@ -13,5 +13,16 @@
             Console.WriteLine(registry.ToString());
             Console.ReadLine();
         }
+
+        /* Next steps:
+         * Next I would add a few functions here to control the interactive
+         * program flow. It would consist of:
+         * - 1. A function to add a new employee to the registry by providing
+         *      name and salary at the command line;
+         * - 2. A function to display the current registry; and
+         * - 3. A function to display a menu and ask the user to choose one of the above.
+         * Then Main would consist of the interactivity needed to run these three functions.
+         * Unfortunately I am out of time for the assignment but hope to come back to this later.
+         */
     }
 }
