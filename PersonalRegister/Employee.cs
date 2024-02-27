577e84b313234901c023abef2089a6478c19c358
jarethholt <jareth.holt@gmail.com>
2024-02-27 15:06:26 +0100

    feat: Added Employee class
    Added the Employee class, including its primary attributes, constructor, and string representation.

---
+++ PersonalRegister/Employee.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalRegister
+{
+    public class Employee
+    {
+        /* Representation of an employee.
+         * 
+         * To represent an employee, we need two pieces of information:
+         * - Their name, either as a whole name or as first and last name;
+         * - Their salary.
+         * These are encapsulated in the attributes Name and Salary.
+         * The relevant methods to expose are:
+         * - The constructor for creating employees (Employee);
+         * - Ways to get/set the name and salary of existing employees;
+         * - A string representation of the employee.
+         * The get/set methods are part of the attribute definitions.
+         * The string representation is defined by ToString (inherited from Object).
+         */
+
+        private const string _SalarySuffix = "SEK/månad";
+        private string _Name;
+        private decimal _Salary;
+
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        public decimal Salary
+        {
+            get { return _Salary; }
+            set
+            {
+                // Should check that salary is positive.
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        paramName: nameof(value),
+                        message: "The salary value must be positive");
+                }
+                _Salary = value;
+            }
+        }
+
+        public Employee(string name, decimal salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+
+        public string ToString()
+        {
+            return $"{Name}, {Salary:C} {_SalarySuffix}";
+        }
+    }
+}
